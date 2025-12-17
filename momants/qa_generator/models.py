015b149f848fdad20d34c38c225b87055faf98ff
Alex Leestemaker <alexleestemaker@gmail.com>
2025-12-17 15:04:41 +0100

    Having to shuffle models around for import reasons

--- momants/qa_generator/models.py
+++ momants/qa_generator/models.py
@@ -1,13 +1,13 @@
 ﻿from django.db import models
 
 
-class Page(models.Model):
+class PageModel(models.Model):
     page_url = models.URLField()
 
-class Question(models.Model):
-    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+class QuestionModel(models.Model):
+    page = models.ForeignKey(PageModel, on_delete=models.CASCADE)
     question_text = models.TextField()
 
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+class AnswerModel(models.Model):
+    question = models.ForeignKey(QuestionModel, on_delete=models.CASCADE)
     answer_text = models.TextField()
