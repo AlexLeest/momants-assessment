bb4edf9587af40ae21bab8a20ff7b35ce70790df
Alex Leestemaker <alexleestemaker@gmail.com>
2025-12-17 14:43:42 +0100

    basic model setup

--- momants/alex/settings.py
+++ momants/alex/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'qa_generator.apps.QaGeneratorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ momants/qa_generator/models.py
@@ -0,0 +1,13 @@
+﻿from django.db import models
+
+
+class Page(models.Model):
+    page_url = models.URLField()
+
+class Question(models.Model):
+    page = models.ForeignKey(Page, on_delete=models.CASCADE)
+    question_text = models.TextField()
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.TextField()
